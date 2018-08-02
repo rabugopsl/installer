d518f107c6bfcc3d15de291f1e5ce7f343c0df5b
rabugopsl <rbustamanteg@psl.com.co>
2018-08-01 19:54:45 -0500

    Corrected directory paths

--- hack/yaml-lint.sh
+++ hack/yaml-lint.sh
@@ -1,5 +1,5 @@
 if [ "$IS_CONTAINER" != "" ]; then 
-  yamllint --config-data "{extends: default, rules: {line-length: {level: warning, max: 120}}}" /workdir/examples/ /workdir/installer/
+  yamllint --config-data "{extends: default, rules: {line-length: {level: warning, max: 120}}}" ./examples/ ./installer/
 else
-  docker run -t -v $(pwd):/workdir --env IS_CONTAINER='TRUE' --entrypoint sh quay.io/coreos/yamllint /workdir/hack/yaml-lint.sh
+  docker run -t -v $(pwd):/workdir --env IS_CONTAINER='TRUE' --entrypoint sh quay.io/coreos/yamllint ./hack/yaml-lint.sh
 fi;
